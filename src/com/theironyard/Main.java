e79fd59d9fac75f0ae88bc6cbbd22983acdcb0af
Steven Burris <stevenburris.jr@gmail.com>
2016-10-17 15:22:14 -0400

    Imported angular file;

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,17 @@
 package com.theironyard;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    static void createTables (Connection conn) throws SQLException {
+        Statement stmt = conn.createStatement();
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR)");
     }
 }
