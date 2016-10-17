3bc9865a8725abf8226f12dbf9323837af16fcb5
Steven Burris <stevenburris.jr@gmail.com>
2016-10-17 15:51:54 -0400

    Added User and UserWrapper java classes

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,17 +1,23 @@
 package com.theironyard;
 
+import spark.Spark;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        createTables(conn);
+        Spark.externalStaticFileLocation("public");
+        Spark.init();
     }
 
     static void createTables (Connection conn) throws SQLException {
         Statement stmt = conn.createStatement();
-        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR)");
+        stmt.execute("CREATE TABLE IF NOT EXISTS users (id IDENTITY, name VARCHAR, address VARCHAR, email VARCHAR)");
     }
 }
---
+++ src/com/theironyard/User.java
@@ -0,0 +1,59 @@
+package com.theironyard;
+
+/**
+ * Created by stevenburris on 10/17/16.
+ */
+public class User {
+    int id;
+    String name;
+    String address;
+    String email;
+
+    public User() {
+    }
+
+    public User(int id, String name, String address, String email) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public User(String name, String address, String email) {
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
