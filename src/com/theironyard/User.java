f54e3fbd9eed2d269cc85c325a4ce137fde1e272
Steven Burris <stevenburris.jr@gmail.com>
2016-10-18 23:04:39 -0400

    Don't know why this doesn't work

--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -33,11 +33,11 @@ public class User {
         this.id = id;
     }
 
-    public String getName() {
+    public String getUsername() {
         return username;
     }
 
-    public void setName(String name) {
+    public void setUsername(String name) {
         this.username = name;
     }
 
